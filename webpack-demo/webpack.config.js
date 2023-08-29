34f37fb43ea147aafa7420cde923e9dbc4f5020b
bubucuo <276185876@qq.com>
2023-08-29 15:35:53 +0800

    save

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,12 +1,29 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   // entry: "./src/index.js",
   entry: {
-    index: "./src/index.js",
+    // index: "./src/index.js",
     print: "./src/print.js",
+    // another: "./src/another-module.js",
+
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
+    },
+    another: {
+      import: "./src/another-module.js",
+      dependOn: "shared",
+    },
+    shared: "lodash",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
   },
   output: {
     // filename: "main.js",
@@ -15,11 +32,13 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        // use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -28,6 +47,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     // 可以修改html页面title
     new HtmlWebpackPlugin({
       title: "Development",
