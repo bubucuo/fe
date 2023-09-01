85554eccb89b88ec100b4de4ff94bdb5f136f21c
bubucuo <276185876@qq.com>
2023-09-01 09:06:29 +0800

    save ts

---
+++ ts-demo/index.js
@@ -0,0 +1,12 @@
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    return UserAccount;
+}());
+var user = new UserAccount("Murphy", 1);
+console.log("user", user); //sy-log
